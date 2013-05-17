aae3cf891de94943c62402832e0cc72f3042658a
jompon <jompon_ger@hotmail.com>
2013-05-18 02:08:49 +0700

    Create src\Model\Main.java

---
+++ src\Model\Main.java
@@ -0,0 +1,18 @@
+package Model;
+
+import View.Play;
+
+
+public class Main {
+
+  /**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Othello othello = Othello.getInstance();
+		Play play = new Play(othello);
+		play.run();
+	}
+
+}
