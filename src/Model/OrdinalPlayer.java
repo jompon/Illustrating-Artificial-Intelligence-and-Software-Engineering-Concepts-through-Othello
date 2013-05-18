829eed0ddd0da79a29d48e9b7944c50ac90fd505
jompon <jompon_ger@hotmail.com>
2013-05-18 18:21:11 +0700

    Create OrdinalPlayer.java

---
+++ src/Model/OrdinalPlayer.java
@@ -0,0 +1,65 @@
+package Model;
+
+/**
+ * OrdinalPlayer is model on one player
+ */
+public class OrdinalPlayer {
+
+  private static int number = 0;		// number of player
+	private int ordinalNumber;			// ordinal of player
+	private char piece;					// color of player
+	private String typePlayer = "";		// type of player
+	private int IdPlayer;				// id number is indicating for player type
+	public OrdinalPlayer(char piece, String typePlayer)
+	{
+		this.piece = piece;
+		this.typePlayer = typePlayer;
+		setOrdinalNumber();
+		setIdPlayer();
+	}
+	public static void newGame( )
+	{
+		number = 0;
+	}
+	public String getType( )
+	{
+		return typePlayer;
+	}
+	
+	public char getPiece()
+	{
+		return piece;
+	}
+	
+	public String getColorPiece()
+	{
+		if( piece == 'O' )			return "White";
+		else if( piece == 'X' )		return "Black";
+		return "It has no color";
+	}
+	
+	public int getIdPlayer( )
+	{
+		return IdPlayer;
+	}
+	private void setIdPlayer( )
+	{
+		if( typePlayer.equals("Computer") )		IdPlayer = 0;
+		else if( typePlayer.equals("Human") )	IdPlayer = 1;
+		else									IdPlayer = 9;
+	}
+	
+	public int getOrdinalNumber()
+	{
+		return ordinalNumber;
+	}
+	private void setOrdinalNumber( )
+	{
+		ordinalNumber = ++number;
+	}
+
+	public String toString( )
+	{
+		return ordinalNumber + "P Player";
+	}
+}
