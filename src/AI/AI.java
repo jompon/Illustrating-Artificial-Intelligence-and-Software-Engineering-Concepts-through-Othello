704c78ea197dd6e17569007cd6024308f006554a
jompon <jompon_ger@hotmail.com>
2013-05-21 22:33:16 +0700

    Update AI.java

--- src/AI/AI.java
+++ src/AI/AI.java
@@ -3,10 +3,7 @@ package AI;
 import java.awt.Point;
 
 public interface AI {
-  
-	public void Think();
-	public Point Place( );
-	public void resetListNode( );
-	public void setCapacity(int capacity);
-	public void setTable(char[][] button);
+	
+	public Point Place(char[][] button);
+	public void clearList( );
 }
